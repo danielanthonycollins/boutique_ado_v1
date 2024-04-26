926f41b07ee623ca98b16f3de7935db50517bd9b
Daniel Collins <danielanthonycollins@gmail.com>
2024-04-26 16:23:46 +0000

    Add templates

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -42,7 +42,8 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     # Optional -- requires install using `django-allauth[socialacocunt]`.
-    'allauth.socialaccount'
+    'allauth.socialaccount',
+    'home'
 ]
 
 MIDDLEWARE = [
@@ -60,7 +61,10 @@ ROOT_URLCONF = 'boutique_ado.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+            os.path.join(BASE_DIR, 'templates', 'allauth')
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
